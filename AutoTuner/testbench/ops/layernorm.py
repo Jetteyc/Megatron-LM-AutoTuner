c9e3662051b08b0ebdc5d0d7bb74f83f7b1128d2
Jetteyc <9149xkcd@gmail.com>
2025-11-22 20:26:30 +0800

    fix: add zero_grad method to resolve AttributeError

--- AutoTuner/testbench/ops/layernorm.py
+++ AutoTuner/testbench/ops/layernorm.py
@@ -14,44 +14,28 @@ from AutoTuner.utils.nvtx import nvtx_decorator, nvtx_range_pop, nvtx_range_push
 from .common import CommonOpsForTest
 from megatron.core.extensions.transformer_engine import TENorm
 
-class LayerNormForTest(CommonOpsForTest):
+class LayerNormForTest(torch.nn.Module, CommonOpsForTest):
     def __init__(
         self,
         tf_config: TransformerConfig,
         hf_config: PretrainedConfig,
         hook_activation=False,
     ):
+        torch.nn.Module.__init__(self)
         CommonOpsForTest.__init__(
             self,
             hook_activation=hook_activation,
             module_name="LayerNorm",
             logging_level=logging.INFO,
         )
-        normalization_type = tf_config.normalization
-        if tf_config.transformer_impl == "transformer_engine":
-            self.norm = TENorm(
-                config=tf_config,
-                hidden_size=hf_config.hidden_size,
-                eps=tf_config.layernorm_epsilon,
-            )
-        else:
-            if normalization_type == "LayerNorm":
-                self.norm = FusedLayerNorm(
-                        config=tf_config,
-                        hidden_size=hf_config.hidden_size,
-                        eps=tf_config.layernorm_epsilon,
-                        persist_layer_norm=tf_config.persist_layer_norm,
-                        zero_centered_gamma=tf_config.layernorm_zero_centered_gamma,
-                    )
-            else:
-                self.norm = WrappedTorchNorm(
-                    config=tf_config,
-                    hidden_size=hf_config.hidden_size,
-                    eps=tf_config.layernorm_epsilon,
-                )
-    
-    def __call__(self, *args, **kwargs):
-        return self.forward(*args, **kwargs)
+
+        if tf_config.transformer_impl != "transformer_engine":
+            raise ValueError(f"Expected transformer_impl to be 'transformer_engine', got '{tf_config.transformer_impl}'")
+        self.norm = TENorm(
+            config=tf_config,
+            hidden_size=hf_config.hidden_size,
+            eps=tf_config.layernorm_epsilon,
+        )
 
     @nvtx_decorator(message="LayerNorm forward")
     def _forward(self, hidden_states: Tensor) -> Tensor:

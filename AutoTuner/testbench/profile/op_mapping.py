6a8aa5b87d47f135427cd7089394602c27a04e8a
Jetteyc <131692829+Jetteyc@users.noreply.github.com>
2025-11-25 16:47:23 +0800

    feat: implement GPTModel operator (#33)

--- AutoTuner/testbench/profile/op_mapping.py
+++ AutoTuner/testbench/profile/op_mapping.py
@@ -1,6 +1,7 @@
 from AutoTuner.testbench.ops_test.decoder_test_hidden import TestDecoderWithHiddenInputs
 from AutoTuner.testbench.ops_test.embedding_test import TestLanguageModelEmbedding
 from AutoTuner.testbench.ops_test.layernorm_test import TestLayerNorm
+from AutoTuner.testbench.ops_test.gpt_model_test import TestGPTModel
 from AutoTuner.testbench.ops_test.preprocess_test import TestPreprocess
 from AutoTuner.testbench.ops_test.self_attention_test import TestSelfAttention
 
@@ -9,5 +10,6 @@ OP_TEST_MAPPING = {
     "Preprocess": TestPreprocess,
     "Decoder": TestDecoderWithHiddenInputs,
     "LayerNorm": TestLayerNorm,
+    "GPTModel": TestGPTModel,
     "SelfAttention": TestSelfAttention,
 }

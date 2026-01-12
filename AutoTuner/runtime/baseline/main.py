f9e69697163ba36c7aefcf30a3cccbc1e0017374
Jetteyc <9149xkcd@gmail.com>
2026-01-12 10:54:36 +0800

    try

--- AutoTuner/runtime/baseline/main.py
+++ AutoTuner/runtime/baseline/main.py
@@ -10,8 +10,26 @@ from megatron.core import parallel_state as mpu
 from transformers import PretrainedConfig
 from transformers import AutoConfig
 from verl.trainer.main_ppo import create_rl_sampler
+from torchdata.stateful_dataloader import StatefulDataLoader
 
-    
+
+
+from torch.utils.data import IterableDataset
+
+class DataSetsGeneratorDataset(IterableDataset):
+    def __init__(self, datasets: DataSets, test_cases):
+        self.datasets = datasets
+        self.test_cases = test_cases
+
+    def __iter__(self):
+        for test_case in self.test_cases:
+            batch_gen = self.datasets.get_batch_generator(test_case)
+            for batch in batch_gen:
+                yield {
+                    "test_case": test_case,
+                    "batch": batch,
+                }
+
 def create_test_cases(config, seqlen):
     json_test_cases = {
         "model": "deepseek-ai/DeepSeek-V3-Base",
@@ -51,7 +69,7 @@ def run(config):
     actor = ActorSimpleRuntimeWorker(config.actor_rollout_ref)
     actor.init_model()
     test_cases = create_test_cases(config, seqlen=1024)
-    train_dataset = DataSets(
+    dataset = DataSets(
         AutoConfig.from_pretrained(config.actor_rollout_ref.model.hf_config_path if config.actor_rollout_ref.model.hf_config_path else config.actor_rollout_ref.model.path),
         test_cases,
         fix_compute_amount=config.fix_compute_amount,
@@ -59,28 +77,39 @@ def run(config):
         vpp_size=mpu.get_virtual_pipeline_model_parallel_world_size(),
     )
     print("---------------------created train dataset---------------------")
-    from verl.utils.fs import copy_to_local
-    local_path = copy_to_local(
-        config.actor_rollout_ref.model.path, use_shm=config.actor_rollout_ref.model.get("use_shm", False)
+    # from verl.utils.fs import copy_to_local
+    # local_path = copy_to_local(
+    #     config.actor_rollout_ref.model.path, use_shm=config.actor_rollout_ref.model.get("use_shm", False)
+    # )
+
+    # # Instantiate the tokenizer and processor.
+    # from verl.utils import hf_processor, hf_tokenizer
+
+    # trust_remote_code = config.data.get("trust_remote_code", False)
+    # tokenizer = hf_tokenizer(local_path, trust_remote_code=trust_remote_code)
+    # # Used for multimodal LLM, could be None
+    # processor = hf_processor(local_path, trust_remote_code=trust_remote_code, use_fast=True)
+    # from verl.utils.dataset.rl_dataset import collate_fn
+    # train_sampler = create_rl_sampler(config.data, train_dataset)
+    # train_dataloader = create_train_dataloader(
+    #     config, 
+    #     train_dataset, 
+    #     tokenizer, 
+    #     processor, 
+    #     collate_fn, 
+    #     train_sampler
+    # )
+    train_dataset = DataSetsGeneratorDataset(
+        datasets=dataset,
+        test_cases=test_cases,
+    )
+    train_dataloader = StatefulDataLoader(
+        dataset=train_dataset,
+        batch_size=1,
+        num_workers=0,
+        drop_last=True,
     )
 
-    # Instantiate the tokenizer and processor.
-    from verl.utils import hf_processor, hf_tokenizer
-
-    trust_remote_code = config.data.get("trust_remote_code", False)
-    tokenizer = hf_tokenizer(local_path, trust_remote_code=trust_remote_code)
-    # Used for multimodal LLM, could be None
-    processor = hf_processor(local_path, trust_remote_code=trust_remote_code, use_fast=True)
-    from verl.utils.dataset.rl_dataset import collate_fn
-    train_sampler = create_rl_sampler(config.data, train_dataset)
-    train_dataloader = create_train_dataloader(
-        config, 
-        train_dataset, 
-        tokenizer, 
-        processor, 
-        collate_fn, 
-        train_sampler
-    )
     print("---------------------created train dataloader---------------------")
 
 
